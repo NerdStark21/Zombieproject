e395e31aa871d6b8f8d55e9abc4ed3b124a67f6a
Aurelien Turpin <aurelien.turpin@telecom-st-etienne.fr>
2018-01-19 15:01:03 +0100

    Les tombes lv1 sont opérationnels

--- game2.js
+++ game2.js
@@ -2,44 +2,44 @@ var speed = {"lv1": 4, "lv2": 3, "lv3": 2, "lv4": 1};
 
 
 var joueur={
-	/*
-	 * Attributs :
-	 * pv
-	 * score
-	 * hitpoint
-	 */
+    /*
+     * Attributs :
+     * pv
+     * score
+     * hitpoint
+     */
 
-	//initialise le joueur
-	init: function(pv,score,hitpoint){
-		this.pv=pv;
-		this.score=score;
-		this.hitpoint=hitpoint;
-	},
+    //initialise le joueur
+    init: function(pv,score,hitpoint){
+        this.pv=pv;
+        this.score=score;
+        this.hitpoint=hitpoint;
+    },
 
     // Incremente le score du joueur de la quantité "score" définit en récupérant zombie.getReward()
     incrementScore: function(score){this.score += score;},
 
     // Retourne true si le joueur n'as plus de vie
     estTouche: function(hitPoint){
-    	if(this.pv - hitPoint <= 0){this.pv = 0; return true;}
-    	else{this.pv -= hitPoint; return false;}
+        if(this.pv - hitPoint <= 0){this.pv = 0; return true;}
+        else{this.pv -= hitPoint; return false;}
     }
 }
 
 
 var zombie ={
-	/*
-	 * Attributs : 
-	 * etatMarche
-	 * imgZombie
-	 * imgGrave
-	 * posX
-	 * posY
-	 * level
-	 * PV
-	 * reward
-	 * speed
-	 */
+    /*
+     * Attributs : 
+     * etatMarche
+     * imgZombie
+     * imgGrave
+     * posX
+     * posY
+     * level
+     * PV
+     * reward
+     * speed
+     */
 
     // Pour savoir quelle image de marche afficher
     etatMarche: 0,
@@ -49,42 +49,42 @@ var zombie ={
     Notation des vitesses : 1 <=> rapide    2 <=> modérée   3 <=> lente     4 <=>très lente
     */
     init: function(imgZ,level, coordPopX, coordPopY){
-    	if(level <= 0 && level >= 5){
-    		console.log("Problème dans la définition de niveau, le zombie doit avoir un niveau compris entre 1 et 4 !")
-    	}
-    	else{
-    		
-    		// On déclare les variables pour les images du zombie et de sa tombe
-    		this.imgZombie = imgZ;
-    		//this.imgGrave = imgG;
+        if(level <= 0 && level >= 5){
+            console.log("Problème dans la définition de niveau, le zombie doit avoir un niveau compris entre 1 et 4 !")
+        }
+        else{
+            
+            // On déclare les variables pour les images du zombie et de sa tombe
+            this.imgZombie = imgZ;
+            //this.imgGrave = imgG;
 
 
-        	// Coord de la position actuelle du zombie
-        	this.posX = coordPopX;
-        	this.posY = coordPopY;
+            // Coord de la position actuelle du zombie
+            this.posX = coordPopX;
+            this.posY = coordPopY;
 
-        	// Niveau du zombie
-        	this.level = level;
+            // Niveau du zombie
+            this.level = level;
 
-        	// On va maintenant définir les stats et les images du zombie en fonction du niveau qu'on lui a mis
-        	if(this.level == 1){
-        		this.pv = 1; this.reward = 1; this.speed = speed["lv1"];
-        	}
-        	else if(this.level == 2){
-        		this.pv = 2; this.reward = 3; this.speed = speed["lv2"];
-        	}
-        	else if(this.level == 3){
-        		this.pv = 3; this.reward = 5; this.speed = speed["lv3"];
-        	}
-        	else{
-        		this.pv = 25; this.reward = 30; this.speed = speed["lv4"];
-        	}
+            // On va maintenant définir les stats et les images du zombie en fonction du niveau qu'on lui a mis
+            if(this.level == 1){
+                this.pv = 1; this.reward = 1; this.speed = speed["lv1"];
+            }
+            else if(this.level == 2){
+                this.pv = 2; this.reward = 3; this.speed = speed["lv2"];
+            }
+            else if(this.level == 3){
+                this.pv = 3; this.reward = 5; this.speed = speed["lv3"];
+            }
+            else{
+                this.pv = 25; this.reward = 30; this.speed = speed["lv4"];
+            }
         }
     },
 
     // Lorsqu'on attaque le zombie
     isAttack: function(hitpoint){
-    	this.pv-=hitpoint;
+        this.pv-=hitpoint;
     },
 
     // Fais avancer le zombie
@@ -108,12 +108,12 @@ var context=grass.getContext("2d");
 
 
 var drawFond =function(){
-	context.clearRect(0,0,800,800);
-	for(var i=0; i<2; i++){
-		for (var j=0;j<2; j++){
-			context.drawImage(fond, 512*i,512*j);
-		}
-	}
+    context.clearRect(0,0,800,800);
+    for(var i=0; i<2; i++){
+        for (var j=0;j<2; j++){
+            context.drawImage(fond, 512*i,512*j);
+        }
+    }
 }
 
 var fond= new Image();
@@ -251,10 +251,10 @@ window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequest
 var start = null;
 var create=false;
 var compteurInc = function (timestamp) {
-	if (start === null) {
-		start = timestamp;
-	}
-	if (timestamp - start >= 1000) {
+    if (start === null) {
+        start = timestamp;
+    }
+    if (timestamp - start >= 1000) {
 
         if(allr){
             //On tire les coordonnées initiale pour chaque zombie
@@ -265,7 +265,7 @@ var compteurInc = function (timestamp) {
 
             var zombie=[img_lvl1,coordPopX,coordPopY, imgGrave, y_init, compteur];
             zombies.push(zombie);
-            console.log(zombies);
+            //console.log(zombies);
 
 /*
             z=Object.create(zombie);
@@ -322,8 +322,8 @@ var compteurInc = function (timestamp) {
         // On l'ajoute a la liste des zombies present
         zombies.push(z);
         console.log('pop');}
-		
-		zombies.onload=drawGame();
+        
+        zombies.onload=drawGame();
         */
         start = timestamp;
     }
